6d8b05143a67eef842e0f53d1734b9395edfb5c3
Karloku Sang <karloku@gmail.com>
2016-03-22 14:32:56 +0800

    rm erd task

--- app/uploaders/csv_uploader.rb
+++ app/uploaders/csv_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 class CsvUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -47,5 +47,4 @@ class CsvUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 Config.setup do |config|
   # Name of the constat exposing loaded settings
   config.const_name = 'Settings'
