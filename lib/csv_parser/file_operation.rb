66434f16512c8a960b786a710f01bae3303a69c6
Karloku Sang <karloku@gmail.com>
2016-03-28 10:39:58 +0800

    quick parse: , ; \t delimiter

--- lib/csv_parser/file_operation.rb
+++ lib/csv_parser/file_operation.rb
@@ -2,10 +2,11 @@
 class CSVParser
   # Handling csv file
   module FileOperation
-    attr_accessor :encoding
+    attr_accessor :encoding, :delimiter
 
     DETECTING_BUFFER_SIZE = 1.kilobytes
     CHUNK_SIZE = 20
+
     def encoding
       @encoding ||=
         File.open(csv_file.csv.current_path, 'r') do |csv|
@@ -13,10 +14,16 @@ class CSVParser
         end
     end
 
+    def delimiter
+      @delimiter ||= /[\,\;\t]/
+    end
+
     def open_csv(&block)
       SmarterCSV.process(csv_file.csv.current_path, chunk_size: CHUNK_SIZE,
                                                     convert_values_to_numeric: true,
                                                     file_encoding: encoding,
+                                                    col_sep: delimiter,
+                                                    row_sep: :auto,
                          &block)
     end
   end
